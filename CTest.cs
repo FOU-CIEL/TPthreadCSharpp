7f71bcd3b1dd31635e41ec38e536451f87bbeebf
axel Fourreau <fourreauaxel@gmail.com>
2026-01-08 11:18:10 +0100

    commit #3

---
+++ CTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPthreadCSharpp
+{
+    namespace CTest
+    {
+        public class CTest
+        {
+            public void AfficheB()
+            {
+                while (true)
+                {
+                    Console.WriteLine("AfficheB thread : " + Thread.CurrentThread.Name);
+                    Thread.Sleep(1000);
+                }
+            }
+
+            public void AfficheC()
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    Console.WriteLine("AfficheC thread : " + Thread.CurrentThread.Name + " iteration " + i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace TPthreadCSharpp
 {
+
+    using CTest;
+
     class Program
     {
         static void AfficheA()
@@ -29,14 +32,31 @@ namespace TPthreadCSharpp
             Thread thread1_fonction = new Thread(AfficheA);
             Thread thread2_fonction = new Thread(AfficheA);
             Thread thread3_fonction = new Thread(AfficheA);
+            CTest.CTest test = new CTest.CTest();
+
+            Thread thread1_fonction1 = new Thread(AfficheA);
+            Thread thread2_fonction1 = new Thread(AfficheA);
+            Thread thread3_fonction1 = new Thread(AfficheA);
+            Thread thread4_fonction2 = new Thread(test.AfficheB);
+            Thread thread5_fonction3 = new Thread(test.AfficheC);
 
             thread1_fonction.Name = "thread1_fonction1";
             thread2_fonction.Name = "thread2_fonction1";
             thread3_fonction.Name = "thread3_fonction1";
+            thread1_fonction1.Name = "thread1_fonction1";
+            thread2_fonction1.Name = "thread2_fonction1";
+            thread3_fonction1.Name = "thread3_fonction1";
+            thread4_fonction2.Name = "thread4_fonction2";
+            thread5_fonction3.Name = "thread5_fonction3";
 
             thread1_fonction.Start();
             thread2_fonction.Start();
             thread3_fonction.Start();
+            //thread1_fonction1.Start();
+            //thread2_fonction1.Start();
+            //thread3_fonction1.Start();
+            thread4_fonction2.Start();
+            thread5_fonction3.Start();
         }
     }
 }
