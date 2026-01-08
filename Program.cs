589459e8a78d13663a2b2a7a127a267120e6837b
axel Fourreau <fourreauaxel@gmail.com>
2026-01-08 09:50:55 +0100

    commit #2

--- Program.cs
+++ Program.cs
@@ -2,14 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TPthreadCSharpp
 {
-    internal class Program
+    class Program
     {
+        static void AfficheA()
+        {
+            while (true)
+            {
+                Console.WriteLine("AfficheA thread : " + Thread.CurrentThread.Name);
+                Thread.Sleep(1000);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(Thread.CurrentThread.Name);
+            Console.WriteLine(Thread.CurrentThread.ThreadState);
+
+            Thread.CurrentThread.Name = "Thread 1";
+            Console.WriteLine(Thread.CurrentThread.Name);
+
+            Thread thread1_fonction = new Thread(AfficheA);
+            Thread thread2_fonction = new Thread(AfficheA);
+            Thread thread3_fonction = new Thread(AfficheA);
+
+            thread1_fonction.Name = "thread1_fonction1";
+            thread2_fonction.Name = "thread2_fonction1";
+            thread3_fonction.Name = "thread3_fonction1";
+
+            thread1_fonction.Start();
+            thread2_fonction.Start();
+            thread3_fonction.Start();
         }
     }
 }
